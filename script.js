1685f7f008a797407c78eeafc9c1b1552d2cdcdb
tahdi-islam <tshdi60@gmail.com>
2022-09-15 08:10:44 +0600

    4th bug fix

--- script.js
+++ script.js
@@ -135,7 +135,9 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpentInt = parseInt(timeSpent)
 
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${
+    startTime ? timeSpentInt : 0
+  } seconds`;
 }, 1000);
